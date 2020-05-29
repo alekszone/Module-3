6d49fc661ba3c8a3394f0cbe5ce3b8958fdc7296
alekszone <50597224+alekszone@users.noreply.github.com>
2020-05-29 17:02:28 +0200

    dedf

---
+++ day 10/java.js
@@ -0,0 +1,55 @@
+const link = "https://striveschool.herokuapp.com/api/movies/";
+const myDetails = window.btoa('user2:4Lr9nqHZZZDNUy7L')
+
+let get = async () => {
+    let response = await fetch(link , {
+      headers: new Headers({
+        "Authorization":"Basic " + myDetails
+      }),
+  });
+  return await response.json();
+
+}
+
+const getProduct = async (select) => {
+  let response = await fetch(link + select, {
+    headers: new Headers({
+    "Authorization":"Basic " + myDetails
+  }),
+})
+  return await response.json();
+};
+const save = async (product) => {
+    let response = await fetch(link, {
+      method: "POST",
+      body: JSON.stringify(product),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + myDetails
+      }),
+    });
+    return response;
+  };
+  const editProduct = async (id, product) => {
+    let response = await fetch(link + id, {
+      method: "PUT",
+      body: JSON.stringify(product),
+      headers: new Headers({
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + myDetails
+      }),
+    });
+    return response;
+  };
+  const deleteProduct = async (id) => {
+    let response = await fetch(link + id, {
+      method: "DELETE",
+      headers: new Headers({
+        "Authorization":"Basic " + myDetails
+         })
+    });
+    return response;
+  };
+
+
+
